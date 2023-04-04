b8a0f475582d100d2ca2eb73fe17326fccf655c6
Sterling De Jesus <s.dejesusp@gmail.com>
2023-04-04 13:33:12 -0400

    Adding files

---
+++ test.py
@@ -0,0 +1,46 @@
+import os, subprocess
+
+TEST_DIR = "/tests"
+CODE_FILE = "main.c"
+COMPILER_TIMEOUT = 10.0
+RUN_TIMEOUT = 10.0
+
+
+code_path = os.path.join(TEST_DIR, CODE_FILE)
+app_path = os.path.join(TEST_DIR, "app")
+
+print("Building...")
+
+try:
+	ret = subprocess.run(["gcc", code_path, "-o", app_path],
+				stdout=subprocess.PIPE,
+				stderr=subprocess.PIPE,
+				timeout=COMPILER_TIMEOUT)
+except Exception as e:
+	print("Error: Compilation failed.", str(e))
+	exit(1)
+
+output = ret.stdout.decode("utf-8")
+print(output)
+output  = ret.stderr.decode("utf-8")
+print(output)
+
+if ret.returncode != 0:
+	print("Compilation failed!")
+	exit(1)
+
+print("Running...")
+try:
+	ret = subprocess.run([app_path],
+				stdout=subprocess.PIPE,
+				timeout=RUN_TIMEOUT)
+except Exception as e:
+	print("Error: Runtime failed", str(e))
+	exit(1)
+
+output = ret.stdout.decode("utf-8")
+print("Output:", output)
+
+# All tests passed!
+print("All tests passed")
+exit(0)
